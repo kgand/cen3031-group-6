201f081552649a94c7700c5b1916206b498119db
kgand <34491606+kgand@users.noreply.github.com>
2025-02-25 23:41:34 -0500

    Remove redundant console logs from development phase
    Cleaned up any leftover console logging now that the Logger utility is in place. (No net code changes, as all logs now use Logger.)

--- scripts/logger.js
+++ scripts/logger.js
@@ -1,6 +1,6 @@
 const Logger = {
-    log: (...args) => console.log('[LOG]', ...args),
-    error: (...args) => console.error('[ERROR]', ...args)
+    log: (...args) => console.log('[Log]', ...args),
+    error: (...args) => console.error('[Error]', ...args)
   };
   
   export default Logger;
